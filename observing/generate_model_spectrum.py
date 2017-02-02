observing.generate_model_spectrum: Minor bug in spectrum normalization; fixed wavelength grid in observed space

observing/generate_model_spectrum.py
@@ -15,6 +15,8 @@ import commands
 from astropy.io import ascii as asc
 from astropy.io import fits
 
+import astropy.units as u
+
 import numpy as np
 import array, time, sets
 
@@ -24,7 +26,8 @@ from astropy.table import Table
 
 from locate_em_lines import gaussian, gaussian_R
 
-def main(tab0, R_spec=3000.0, out_path='', silent=False, verbose=True):
+def main(tab0, R_spec=3000.0, out_path='', unit0=u.micron, silent=False,
+         verbose=True):
 
     '''
     Main function() that generates a spectrum containing H-alpha, [NII],
@@ -50,13 +53,16 @@ def main(tab0, R_spec=3000.0, out_path='', silent=False, verbose=True):
     Created by Chun Ly, 1 February 2017
     Modified by Chun Ly, 2 February 2017
      - Minor modification for a small bug
+     - Add out_path keyword option
+     - Add unit0 keyword to specify desired units for wavelength
+     - Fix wavelength grid to be a set wavelength in observed space
+     - Minor bug fix with spectrum normalize (need dl factor)
     '''
     
     if silent == False:
         print '### Begin generate_model_spectrum.main | '+systime()
 
-    x_min, x_max = 6300.0, 7000.0 # in Angstroms
-    x0 = np.arange(x_min, x_max, 0.25) # rest wavelength
+    x_min0, x_max0 = 5000.0, 7000.0 # in Angstroms
     
     n_sources = len(tab0)
 
@@ -69,21 +75,29 @@ def main(tab0, R_spec=3000.0, out_path='', silent=False, verbose=True):
     logNIIHa = tab0['logNIIHa']
     
     scale0 = [1.0, 0.0, 0.1, 0.1]
-    
+
+    wave_scale = (1 * u.Angstrom).to(unit0).value
+
     for ii in range(n_sources):
-        flux = np.zeros(len(x0))
-        wave = (1+zspec[ii])*x0
+        z0 = (1+zspec[ii])
+
+        # Mod on 02/02/2017
+        x_min, x_max = x_min0*z0, x_max0*z0
+        dl = 0.25
+        wave = np.arange(x_min, x_max, dl) # rest wavelength
+
+        flux = np.zeros(len(wave))
 
         for ll in range(len(lambda0)):
-            o_lambda = lambda0[ll] * (1+zspec[ii])
+            o_lambda = lambda0[ll] * z0
             
             s_temp = gaussian_R(wave, o_lambda, R_spec)
             if scale0[ll] != 0.0:
-                flux += s_temp * Ha_flux[ii] * scale0[ll]
+                flux += s_temp * Ha_flux[ii] * dl * scale0[ll]
             else:
-                flux += s_temp * Ha_flux[ii] * 10**(logNIIHa[ii])
+                flux += s_temp * Ha_flux[ii] * dl * 10**(logNIIHa[ii])
 
-        f_table = Table([wave, flux])
+        f_table = Table([wave * wave_scale, flux])
 
         outfile = out_path + ID0[ii]+'.spec.txt'
         if silent == False: print '### Writing : ', outfile
@@ -135,7 +149,8 @@ def gnirs_2017a(silent=False, verbose=True):
                  names=('ID','zspec','Ha_flux','logNIIHa'))
 
     print tab0
-    R_spec = 7200/2.5
+
+    qR_spec = 7200/2.5
 
     out_path = path0 + 'ETC/'
-    main(tab0, R_spec, out_path=out_path)
+    main(tab0, R_spec, out_path=out_path, unit0=u.nm)
d8ca65df3f76b7b4b3e0dffecbad8c0fcf330d8c
Chun Ly <astro.chun@gmail.com>
2017-02-02 13:50:33 -0700

    observing.generate_model_spectrum: Add gnirs_2017a() function

--- observing/generate_model_spectrum.py
+++ observing/generate_model_spectrum.py
@@ -8,7 +8,7 @@ expected ratios.
 
 import sys, os
 
-from chun_codes import systime
+from chun_codes import systime, match_nosort_str
 
 from os.path import exists
 import commands
@@ -22,9 +22,9 @@ import matplotlib.pyplot as plt
 
 from astropy.table import Table
 
-from locate_emission_lines import gaussian, gaussian_R
+from locate_em_lines import gaussian, gaussian_R
 
-def main(tab0, R_spec=3000.0, silent=False, verbose=True):
+def main(tab0, R_spec=3000.0, out_path='', silent=False, verbose=True):
 
     '''
     Main function() that generates a spectrum containing H-alpha, [NII],
@@ -48,6 +48,8 @@ def main(tab0, R_spec=3000.0, silent=False, verbose=True):
     Notes
     -----
     Created by Chun Ly, 1 February 2017
+    Modified by Chun Ly, 2 February 2017
+     - Minor modification for a small bug
     '''
     
     if silent == False:
@@ -77,17 +79,63 @@ def main(tab0, R_spec=3000.0, silent=False, verbose=True):
             
             s_temp = gaussian_R(wave, o_lambda, R_spec)
             if scale0[ll] != 0.0:
-                flux += s_temp * Ha_flux * scale0[ll]
+                flux += s_temp * Ha_flux[ii] * scale0[ll]
             else:
-                flux += s_temp * Ha_flux * 10**(logNIIHa[ii])
+                flux += s_temp * Ha_flux[ii] * 10**(logNIIHa[ii])
 
         f_table = Table([wave, flux])
 
-        outfile = ID0[ii]+'.spec.txt'
+        outfile = out_path + ID0[ii]+'.spec.txt'
         if silent == False: print '### Writing : ', outfile
-        asc.write(f_table, outfile, format='no_header')
+        asc.write(f_table, outfile, format='no_header', overwrite=True)
 
     if silent == False:
         print '### End generate_model_spectrum.main | '+systime()
 #enddef
 
+def gnirs_2017a(silent=False, verbose=True):
+    '''
+    Function to run generate_model_spectrum.main() for Gemini-N/GNIRS
+    targets in 2017A program
+
+    Parameters
+    ----------
+    None
+          
+    Returns
+    -------
+    
+    Notes
+    -----
+    Created by Chun Ly, 2 February 2017
+    '''
+    
+    path0   = '/Users/cly/Dropbox/Observing/2017A/Gemini/'
+    infile1 = path0 + 'targets.2017a.txt'
+
+    path1   = r'/Users/cly/Google Drive/Documents/Proposals/2017A/Gemini/'
+    infile2 = path1 + 'PIT_SDF_DEEP2.bright.txt'
+
+    if silent == False: print '### Reading : ', infile1
+    tab1 = asc.read(infile1, format='commented_header')
+
+    ID    = [str0.replace('*','') for str0 in tab1['ID']]
+    zspec = tab1['redshift']
+
+    if silent == False: print '### Reading : ', infile2
+    tab2 = asc.read(infile2)
+
+    ID2  = [str0.replace('#','') for str0 in tab2['ID']]
+    idx1, idx2 = match_nosort_str(ID, ID2)
+
+    Ha_flux  = tab2['Ha_flux'][idx2]
+    logNIIHa = tab2['logNIIHa'][idx2]
+
+    tab0 = Table([ID, zspec, Ha_flux, logNIIHa],
+                 names=('ID','zspec','Ha_flux','logNIIHa'))
+
+    print tab0
+    R_spec = 7200/2.5
+
+    out_path = path0 + 'ETC/'
+    main(tab0, R_spec, out_path=out_path)

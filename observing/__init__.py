330e3e3aa7d2e8180ed694c4b6396418a638a1b9
Chun Ly <astro.chun@gmail.com>
2017-07-07 15:56:17 -0700

    Add find_nearby_bright_star to init

--- observing/__init__.py
+++ observing/__init__.py
@@ -2,8 +2,10 @@ import get_photometry
 import locate_em_lines
 import sdss_2mass_proper_motion
 import generate_model_spectrum
+import find_nearby_bright_star
 
 reload(get_photometry)
 reload(locate_em_lines)
 reload(sdss_2mass_proper_motion)
 reload(generate_model_spectrum)
+reload(find_nearby_bright_star)

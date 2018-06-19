fc0cbd068f5eb8863adf0280cbf7e65da8f17ea0
Chun Ly <astro.chun@gmail.com>
2018-06-18 20:11:26 -0700

    Change imports for observing __init__

--- observing/__init__.py
+++ observing/__init__.py
@@ -1,11 +1,11 @@
-import get_photometry
-import locate_em_lines
-import sdss_2mass_proper_motion
-import generate_model_spectrum
-import find_nearby_bright_star
+#import get_photometry
+#import locate_em_lines
+#import sdss_2mass_proper_motion
+#import generate_model_spectrum
+#import find_nearby_bright_star
 
-reload(get_photometry)
-reload(locate_em_lines)
-reload(sdss_2mass_proper_motion)
-reload(generate_model_spectrum)
-reload(find_nearby_bright_star)
+#reload(get_photometry)
+#reload(locate_em_lines)
+#reload(sdss_2mass_proper_motion)
+#reload(generate_model_spectrum)
+#reload(find_nearby_bright_star)

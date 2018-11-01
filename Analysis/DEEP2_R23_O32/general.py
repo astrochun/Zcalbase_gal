52a340d7b6ad499b4d869a74046aa746dd0b4ebf
Reagen Leimbach <pigdog815@gmail.com>
2018-11-01 14:10:58 -0700

    Added a function to exclude outlier data

--- Analysis/DEEP2_R23_O32/general.py
+++ Analysis/DEEP2_R23_O32/general.py
@@ -22,7 +22,7 @@ import glob
 
 from Zcalbase_gal.Analysis.DEEP2_R23_O32 import Binning_and_Graphing_MasterGrid, Stackboth_MasterGrid, zoom_and_gauss_general, hstack_tables,  adaptivebinning, Stacking_voronoi, R_temp_calcul, line_ratio_plotting
 
-fitspath='/Users/reagenleimbach/Desktop/Zcalbase_gal/Gridmethod_1018/'
+fitspath='/Users/reagenleimbach/Desktop/Zcalbase_gal/Gridmethod_1029/'
 fitspath_ini = '/Users/reagenleimbach/Desktop/Zcalbase_gal/'
 
 xcoor = [3726.16, 3728.91, 3797.90, 3835.38, 3868.74, 3889.05, 3888.65, 3967.51, 3970.07, 4340.46, 4363.21, 4471.5, 4958.91, 5006.84, 4101.73, 4363.21, 4861.32]
@@ -33,7 +33,14 @@ line_type = ['Oxy2', 'Balmer', 'Single', 'Single', 'Balmer', 'Balmer', 'Single',
 
 line_name = ['OII_3727','H_9','NeIII','HeI','HEPSIL', 'HDELTA', 'OIII_4363', 'HBETA', 'OIII_4958','OIII_5007']   # 'NII_6548', 'HALPHA', 'NII_6584', 'SII_6717', 'SII_6730'] #'H_10' line not fitted because the 'x0 is infeasible' error occurred. In future go back and change para_bounds so that the line canbe fit  4340.46; this is the reason why we have two lists of xcoor and lambda0
 
-
+def exclude_outliers(objno):
+    flag = np.zeros(len(objno), dtype=int)
+    bad_data = np.array(['32007727', '32101412', '42006031', '32035286', '14023705'])
+    for ii in range(len(bad_data)):
+        idx = [xx for xx in range(len(objno)) if bad_data[ii] == str(objno[xx])][0]
+        flag[idx] = 1
+    
+    return flag
 
 def get_det3():
     for ii in range(1,5):
@@ -45,24 +52,43 @@ def get_det3():
             data0 = vstack([data0, data])
     #fits.writeto(fitspath_ini+'f3_0716/DEEP2_Fields_combined', data0)
         #print 'data0 : ', len(data0)
-    O2 = data0['OII_FLUX_MOD']
-    O3 = 1.33*data0['OIIIR_FLUX_MOD']
-    Hb = data0['HB_FLUX_MOD']
-    R23 = (O2+O3)/Hb
-    O32 = O3/O2
-    SNR2 = data0['OII_SNR']
-    SNR3 = data0['OIIIR_SNR']
-    SNRH = data0['HB_SNR']
+
+    objno = data0['OBJNO']
+    exclude_flag = exclude_outliers(objno)
+    print "exclude flag: ", np.where(exclude_flag == 1)
+    
+    O2_ini = data0['OII_FLUX_MOD']
+    O3_ini = 1.33*data0['OIIIR_FLUX_MOD']
+    Hb_ini = data0['HB_FLUX_MOD']
+    R23_ini = (O2_ini+O3_ini)/Hb_ini
+    O32_ini = O3_ini/O2_ini
+
+    SNR2_ini = data0['OII_SNR']
+    SNR3_ini = data0['OIIIR_SNR']
+    SNRH_ini = data0['HB_SNR']
+
+    
     #SNR code: This rules out major outliers by only using specified data
-    det3 = np.where((SNR2 >= 3) & (SNR3 >= 3) & (SNRH >= 3) &
-                    (O2 > 0) & (O3 > 0) & (Hb>0))[0]
+    det3 = np.where((SNR2_ini >= 3) & (SNR3_ini >= 3) & (SNRH_ini >= 3) &
+                    (O2_ini > 0) & (O3_ini > 0) & (Hb_ini>0) & (exclude_flag==0))[0]
 
 
     data3 = data0[det3]
 
-    O2_det3 =O2[det3]
-    O3_det3 =O3[det3]
-    Hb_det3 =Hb[det3]
+    R23 = R23_ini[det3]
+    O32 = O32_ini[det3]
+    Hb  = Hb_ini[det3]
+    O2  = O2_ini[det3]
+    O3  = O3_ini[det3]
+    SNR2 =SNR2_ini[det3]
+    SNR3 =SNR3_ini[det3]
+    SNRH =SNRH_ini[det3]
+    
+    O2_det3 =O2_ini[det3]
+    O3_det3 =O3_ini[det3]
+    Hb_det3 =Hb_ini[det3]
+
+    print 'len(R23)', len(R23), 'len(O2_det3)', len(O2_det3)
     
     return R23, O32, O2, O3, Hb, SNR2, SNR3, SNRH, det3, data3, O2_det3, O3_det3, Hb_det3
 
@@ -100,6 +126,8 @@ def run_R23_O32_analysis(dataset,mask='None'):
         N_arr_grid = grid_data['N_arr0']
         R23_grid = grid_data['R23_grid']
         O32_grid = grid_data['O32_grid']
+        T_arr = grid_data['T_arr']
+        print 'len(T_arr)', len(T_arr)
         
         #Option to Change: Masking the night sky emission lines 
         if mask == True:
@@ -172,8 +200,10 @@ def run_R23_O32_analysis(dataset,mask='None'):
 
         R_temp_calcul.run_function(fitspath, out_ascii, out_fits, pdf_name, combine_flux_ascii)  #combine_fits, header_combine_fits,
 
-        
-
+        temp_table= asc.read(out_ascii)
+        SN_4363 = temp_table['S/N_4363']
+        det_4363 = np.where((SN_4363>=3))[0]
+        print det_4363
 
 
 
@@ -299,89 +329,8 @@ def run_R23_O32_analysis(dataset,mask='None'):
 
         R_temp_calcul.run_function(fitspath, out_ascii, out_fits, pdf_name, combine_flux_ascii)
 
+        temp_table= asc.read(out_ascii)
+        SN_4363 = temp_table['S/N_4363']
+        det_4363 = np.where((SN_4363>=3))[0]
+        print det_4363
 
-
-
-
-
-
-
-'''
-    if dataset == 'Voronoi14': 
-         #Binning and Graphing MasterGrid
-        #Options to Change: Bin Size
-        pdf_pages1 = PdfPages(fitspath+'R23_O32_bin025_scatter_and_hexbin_MasterGrid.pdf')
-        outfile1 = fitspath + 'Arrays_R23O32bin025MasterGrid.npz'
-        R23_bin = 0.25
-        O32_bin = 0.25
-        binstr = 025
-        
-        Binning_and_Graphing_MasterGrid.making_Grid(pdf_pages1,outfile1, O2, O3, Hb, SNR2, SNR3, SNRH, det3, data3, O2_det3, O3_det3, Hb_det3, R23_bin, O32_bin)
-        print 'made Arrays_R23O32bin025MasterGrid.npz, scatter_and_hexbin, testmastergrid(need to find if this is used anywhere)'
-        print 'finished Binning_and_Graphing_MasterGrid'
-
-
-        #Stackboth_MasterGrid
-        #Option to Change: Bin size 
-        outfile01 = fitspath+ 'Arrays_R23O32bin01MasterGrid.npz'
-        outfile025 = fitspath + 'Arrays_R23O32bin025MasterGrid.npz'
-        if R23_bin == 0.25:
-            grid_data = np.load(outfile025)
-        else: grid_data = np.load(outfile01)
-
-        #Option to Change: Masking the night sky emission lines 
-        if mask == True:
-            Stack_name = 'Stacking_Wave_vs_Spect1D_Masked_MasterGrid_bin'+str(binstr)+'.pdf'
-            Stackboth_MasterGrid.run_Stacking_Master_mask(Stack_name)
-        else:
-            Stack_name = 'Stacking_Wave_vs_Spect1D_MasterGrid_bin'+str(binstr)+'.pdf'
-            Stackboth_MasterGrid.run_Stacking_Master(Stack_name)
-
-        #Outfile and pdf both use name
-        print 'finished with stacking,' + Stack_name + 'pdf and fits files created'
-
-
-
-        #Zoom_and_gauss_general
-
-        outfile_grid = r'/Users/reagenleimbach/Desktop/Zcalbase_gal/' + Stack_name
-        stack2D, header = fits.getdata(outfile_grid, header=True)
-        wave = header['CRVAL1'] + header['CDELT1']*np.arange(header['NAXIS1'])
-        Spect_1D = fits.getdata(outfile_grid)
-        dispersion = header['CDELT1']
-
-        #Option to change: Constants used as initial guesses for gaussian fit
-        s=1.0
-        a= 1.0
-        c = 1
-        
-        s1=-0.3
-        a1= 4.7
-        s2 = 1
-        a2 = -1.8
-
-        zoom_and_gauss_general.zm_general(stack2D, header, wave, Spect_1D, dispersion, lambda0, line_type, line_name, s,a,c,s1,a1,s2,a2)
-
-        print 'finished gaussian fitting,' ++ 'created'
-
-        #hstack_table
-        #Option to change: name of new fits file created
-        intro = fitspath + #fill in name
-        asc_into = asc.read(intro)
-        table_files = glob.glob(fitspath )+  #fill in name)
-        combine_flux_table = '/Users/reagenleimbach/Desktop/Zcalbase_gal/Grid_combined_flux_table.fits'
-        hstack_tables.h_stack(table_files, asc_intro, combine_flux_table)
-
-        print 'Grid_combine_flux_table created'
-
-        #line_ratio_plotting
-        #I need to go back through and figure out what is the average and what is the composite
-
-        #R_temp_calcul
-        combine_fits = fits.getdata(combine_flux_table, header = True)
-        out_ascii = fitspath+ '/Grid_temperatures_metalicity.tbl'
-        out_fits = fitspath+ '/Grid_temperatures_metalicity.fits'
-        pdf_name = 'Grid_Temp_Composite_Metallicity'
-
-        R_temp_calcul.run_function(out_ascii, out_fits, pdf_name)
-'''

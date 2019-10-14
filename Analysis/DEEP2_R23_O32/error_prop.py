1d0538a15aa6e931ca96048f13453cb0117d5753
Reagen Leimbach <pigdog815@gmail.com>
2019-10-14 14:17:20 -0700

    Changes made to make code run more smoothly for both projects

--- Analysis/DEEP2_R23_O32/error_prop.py
+++ Analysis/DEEP2_R23_O32/error_prop.py
@@ -29,23 +29,12 @@ from chun_codes import random_pdf, compute_onesig_pdf
 # TM_tab is the table with your metallicities, temperatures, and line ratios
 # flux_tab (for Reagen) is produced by the zoom_and_gauss_general code and has the RMS values for each bin
 # flux_tab (for Caroline) is produced by emission_line_fit code and has the RMS values for each bin
-def error_prop_chuncodes(fitspath, project, dataset):
-    if project == 'R23O32':
-        TM_file = fitspath + dataset + '_temperatures_metalicity.tbl'
-        flux_file = fitspath + dataset + '_combined_flux_table.tbl'
-        
-        
-    if project == 'ML':
-        TM_file = fitspath + dataset + '_derived_properties_metallicity.tbl'
-        flux_file = fitspath + dataset + '_emission_lines.tbl'
-        
-        
-    if project == '': 
-        print('Please specify project')
-        return
+def error_prop_chuncodes(fitspath, flux_file):
+    #TM_file = fitspath + dataset + '_temperatures_metalicity.tbl'
+    #flux_file = fitspath + dataset + '_combined_flux_table.tbl'
+    #TM_file = fitspath + dataset + '_derived_properties_metallicity.tbl'
+    #flux_file = fitspath + dataset + '_emission_lines.tbl'
     
-    
-    TM_tab = asc.read(TM_file)
     combine_flux_tab = asc.read(flux_file)
     
     OII_flux      = combine_flux_tab['OII_3727_Flux_Gaussian'].data
@@ -58,20 +47,13 @@ def error_prop_chuncodes(fitspath, project, dataset):
     OIII4959_RMS  = combine_flux_tab['OIII_4958_RMS'].data
     OIII5007_flux = combine_flux_tab['OIII_5007_Flux_Gaussian'].data
     OIII5007_RMS  = combine_flux_tab['OIII_5007_RMS'].data
-    try:
-        Hgamma_flux   = combine_flux_tab['HGAMMA_Flux_Gaussian'].data
-        Hgamma_RMS    = combine_flux_tab['HGAMMA_RMS'].data
-    except KeyError:
-        Hgamma_flux   = combine_flux_tab['Hgamma_Flux_Gaussian'].data
-        Hgamma_RMS    = combine_flux_tab['Hgamma_RMS'].data
+    Hgamma_flux   = combine_flux_tab['HGAMMA_Flux_Gaussian'].data
+    Hgamma_RMS    = combine_flux_tab['HGAMMA_RMS'].data
         
     
     line_names = ['OII_3727', 'HDELTA', 'HGAMMA', 'OIII_4363', 'OIII_4958', 'OIII_5007']
     flux_data = [OII_flux, Hdelta_flux, Hgamma_flux, OIII4363_flux, OIII4959_flux, OIII5007_flux ]
     RMS_data  = [OII_RMS, Hdelta_RMS, Hgamma_RMS, OIII4363_RMS, OIII4959_RMS, OIII5007_RMS]
-
-    Temp          = TM_tab['Temperature'].data
-    metallicity   = TM_tab['com_O_log'].data
 
     x_arr = np.random.random(1000)
     #p_page= fitspath+dataset+'/'+str(np.int(working_wave))+'error_histogram.pdf'
@@ -90,24 +72,32 @@ def error_prop_chuncodes(fitspath, project, dataset):
         
         print('err_function:', flux_gpdf, flux_gpdf.shape)
         print('err',err, len(err),'xpeak', xpeak,len(err))
-    
-    
-    ##only saves the last line array
-    '''
-    for aa in range(len(flux_data)):
-        flux_gpdf = random_pdf(flux_data[aa],RMS_data[aa], seed_i =1, n_iter=1000, silent = False)
 
-    print('err_function:', flux_gpdf, flux_gpdf.shape)
-
-    
-    for bb in range(len(flux_data)): 
-        err, xpeak= compute_onesig_pdf(flux_gpdf,flux_data[bb], usepeak=False, silent=True, verbose = True)
-    print('err',err, len(err),'xpeak', xpeak,len(err))
-    '''
 
     ###Next Step is to make the curves and graph the functions
-
     
+def plotting_errors(fitspath, TM_file, flux_file):
+       
+    TM_tab = asc.read(TM_file)
+    combine_flux_tab = asc.read(flux_file)
+
+    Temp          = TM_tab['Temperature'].data
+    metallicity   = TM_tab['com_O_log'].data
+
+    OII_flux      = combine_flux_tab['OII_3727_Flux_Gaussian'].data
+    OIII4363_flux = combine_flux_tab['OIII_4363_Flux_Gaussian'].data
+    OIII4959_flux = combine_flux_tab['OIII_4958_Flux_Gaussian'].data
+    OIII5007_flux = combine_flux_tab['OIII_5007_Flux_Gaussian'].data
+
+    OII_Low_Error      = combine_flux_tab['OII_3727_Flux_Gaussian'].data
+    OIII4363_Low_Error = combine_flux_tab['OIII_4363_Flux_Gaussian'].data
+    OIII4959_Low_Error = combine_flux_tab['OIII_4958_Flux_Gaussian'].data
+    OIII5007_Low_Error = combine_flux_tab['OIII_5007_Flux_Gaussian'].data
+    
+    
+    
+
+
     #fig_err, ax = plt.subplots()
     #plt.hist(fluxg_pdf, 50)
 

71fe8aecb6d0d6ec69763511f69eda44470fdabf
Reagen Leimbach <pigdog815@gmail.com>
2019-11-06 10:49:40 -0700

    Working to make code more adaptible; The run function works now, but still working to get the histogram function to work

--- histogram_plots.py
+++ histogram_plots.py
@@ -21,62 +21,81 @@ import sys
 
 
 
-def histogram(path, data_path,table_path, pdf_name , statistical_peaks, table_key='', data_name = ''): 
-    data_all = np.load(data_path)
-    data_hist = data_all[data_name]
-
-    statistical_all = np.load(statistical_peaks)
-    xpeaks = statistical_all[data_name]
-    print xpeaks.shape
-    print data_hist.shape
-
+def histogram(path, data_all,table_path, pdf_name , data_name, xpeak_key, table_key=''):
+    pdf_pages = PdfPages(path+pdf_name)
+    #print data_path
+    #data_all = np.load(data_path)
     tab1 = asc.read(table_path)
     if table_key == 'ID':
         ID = tab1['ID']
         
-        calculated_metal = tab1['com_O_log']
+        calculated_com = tab1['com_O_log']
+        calculated_Te = tab1 ['Temperature']
+        calculated_single = tab1['O_s_ion']
+        calculated_double = tab1['O_d_ion']
         detect = tab1['Detection']
         detection = np.where((detect==1))[0]
-        com_metal = calculated_metal[detection]
         ID_detect = ID[detection]
 
+    '''statistical_all = np.load(statistical_peaks)
+    xpeaks = statistical_all[xpeak_key]
+    print xpeaks.shape
+    print data_hist.shape'''
 
-    pdf_pages = PdfPages(path+pdf_name)
+    for bb in range(len(xpeak_key)):
+        for ii in range(len(data_all)):
+            print bb, ii
+            hist_name = data_name[bb][ii]
+            xpeak_name = xpeak_key[ii]
+            print hist_name, xpeak_name
+            #data_hist = data_all[hist_name]
+            data_xpeak = data_all[xpeak_name]
+            
+            #This is defining a quick fix for passing in several wanted histograms 
+            if hist_name == 'Te_propdist': calculated_value = calculated_Te[detection]
+            if hist_name == 'O_d_ion_pdf': calculated_value = calculated_double[detection]
+            if hist_name == 'O_s_ion_pdf': calculated_value = calculated_single[detection]
+            if hist_name == 'com_O_log_pdf': calculated_value = calculated_com[detection]
+            
+            print(calculated_value)
+            
+            
+            
 
-    nrows = len(com_metal)/2
-    print nrows
-    rows =np.arange(nrows)
-    print 'a', rows
-    ncols = 2
+        nrows = len(calculated_value)/2
+        print nrows
+        rows =np.arange(nrows)
+        print 'a', rows
+        ncols = 2
     
     
-    for aa in range(xpeaks.shape[0]):
-        row = rows[aa/2]
-        col = aa% ncols
-        print row, col
-        if aa % (nrows*ncols) ==0:
-            fig, ax_arr = plt.subplots(nrows = nrows, ncols= ncols, squeeze =False)
+        for aa in range(xpeaks.shape[0]):
+            row = rows[aa/2]
+            col = aa% ncols
+            #print row, col
+            if aa % (nrows*ncols) ==0:
+                fig, ax_arr = plt.subplots(nrows = nrows, ncols= ncols, squeeze =False)
 
-        ax = ax_arr[row,col]
-        min_val = np.nanmin(data_hist[aa])
-        max_val = np.nanmax(data_hist[aa])
-        print min_val, max_val
+            ax = ax_arr[row,col]
+            min_val = np.nanmin(data_hist[aa])
+            max_val = np.nanmax(data_hist[aa])
+            #print min_val, max_val
         
-        bin_arr = np.linspace(min_val, max_val)
-        #print bin_arr
+            bin_arr = np.linspace(min_val, max_val)
+            #print bin_arr
         
-        title ='Bin: ',ID_detect[aa]
-        ax.hist(data_hist[aa], bins =bin_arr)
-        ax.axvline(x = xpeaks[aa],color = 'r', label = 'compute_one_sig_xpeak', linewidth = 0.5)
-        ax.axvline(x = com_metal[aa],color = 'm', label = 'stacked metallicities', linewidth =0.5)
-        ax.set_xlim(7.75,9.15)
-        ax.annotate(title, [0.95,0.5], xycoords = 'axes fraction',va = 'center', ha = 'right', fontsize = 6)
-        plt.subplots_adjust(left= 0.05 , bottom= 0.10 , right= 0.95, top= 0.90, wspace = 0.15, hspace =0.15)
-        if aa%(nrows*ncols) == 0:ax.legend(fontsize = 'xx-small')
-        if row != 3:
-            ax.set_xticklabels([])
-        if row == 3: plt.xlabel("Metallicity") 
-    fig.savefig(pdf_pages, format ='pdf')
+            title ='Bin: ',ID_detect[aa]
+            ax.hist(data_hist[aa], bins =bin_arr)
+            ax.axvline(x = xpeaks[aa],color = 'r', label = 'compute_one_sig_xpeak', linewidth = 0.5)
+            ax.axvline(x = calculated_value[aa],color = 'm', label = 'stacked metallicities', linewidth =0.5)
+            ax.set_xlim(min_val, max_val)
+            ax.annotate(title, [0.95,0.5], xycoords = 'axes fraction',va = 'center', ha = 'right', fontsize = 6)
+            plt.subplots_adjust(left= 0.07 , bottom= 0.10 , right= 0.97, top= 0.97, wspace = 0.15, hspace =0.15)
+            if aa%(nrows*ncols) == 0:ax.legend(fontsize = 'xx-small')
+            if row != 3:
+                ax.set_xticklabels([])
+            if row == 3: plt.xlabel("Metallicity") 
+            fig.savefig(pdf_pages, format ='pdf')
         
     pdf_pages.close()
 
@@ -84,19 +103,63 @@ def histogram(path, data_path,table_path, pdf_name , statistical_peaks, table_ke
 
 
 
-def run_histogram_TM(fitspath,Te_pdf_dict,Te_xpeak_dict, metallicity_pdf_dict, metallicity_xpeak_dict, TM_file):
+def run_histogram_TM(fitspath, TM_file, dict_list, data_name):     
 
-    hist_wanted = ['Te_propdist', 'com_O_log,', 'O_s_ion', 'O_d_ion']
+    #dict_list = [Te_pdf_dict,Te_xpeak_dict, metallicity_pdf_dict, metallicity_xpeak_dict]
+    #data_name = [['Te_propdist'], ['Te_xpeak'],['O_d_ion_pdf', 'O_s_ion_pdf', 'com_O_log_pdf'],['O_s_ion_xpeak','O_d_ion_xpeak', 'com_O_log_xpeak']]
 
+
+    #['O_d_ion_pdf', 'O_s_ion_pdf', 'com_O_log_pdf'],
+    #['O_d_ion_pdf', 'O_s_ion_pdf', 'com_O_log_pdf']]
+
+
+
+    #dictxpeak_key = ['Te_xpeak','O_d_ion_pdf', 'O_s_ion_pdf', 'com_O_log_pdf']
+
+
+
+
+    xpeak_key = ['Te_xpeak','O_s_ion_xpeak','O_d_ion_xpeak', 'com_O_log_xpeak']
+
+    histo_dict = {} #will have all the data and xpeaks for all histograms wanted 
+    for bb in range(len(dict_list)):
+        dic0 = np.load(dict_list[bb])   ###dictionary = np.load(path of the dictionary)
+        #print dic0.keys()
+        for aa in range(len(data_name[bb])):
+            print data_name[bb]
+            print data_name[bb][aa]
+            key_name = data_name[bb][aa]     ###''name of the key
+            #print key_name
+            dic1 = {key_name: dic0[key_name]}             ###{key: dictionary[''name of key]}
+            histo_dict.update(dic1)         ###updates new dictionary 
+    
+    print histo_dict
+
+    
+    pdf_name = 'Te_M_histogram_plots.pdf'
+    histogram(fitspath, histo_dict, TM_file, pdf_name, data_name, xpeak_key, table_key = 'ID')
+
+
+
+    ''''
     for ii in range(len(hist_wanted)):
-        data_name = hist_wanted[ii]
-        print data_name
-        if data_name == 'Te':
+        #data_name = hist_wanted[ii]
+        #print data_name
+        if data_name == 'Te_propdist':
+            print 'Te'
             data_path = Te_pdf_dict
+            print data_path
             statistical_peaks = Te_xpeak_dict
             
         else:
             data_path = metallicity_pdf_dict
             statistical_peaks = metallicity_xpeak_dict
         pdf_name = data_name+'histogram_plots.pdf'
-        histogram(fitspath, data_path, TM_file,pdf_name, statistical_peaks, table_key = 'ID', data_name = data_name)           
+        print pdf_name
+        histogram(fitspath, data_path, TM_file,pdf_name, statistical_peaks, table_key = 'ID', data_name = data_name,xpeak_key= xpeak_key[ii])
+
+        histogram(fitspath, histo_dict, TM_file, pdf_name, table_key = 'ID', data_name, xpeak_key)'''
+
+
+
+###generalize code further so that the run function passes in a list of stuff

Generalized plotting - i.e. number of subplots and plotting on certain rows and columns, changed row calculation to integer division to work with Python 3, and fixed min_val to not include negative infinity.

histogram_plots.py
@@ -82,9 +82,11 @@ def histogram(path, data_all,table_path, pdf_name , xpeak_key, table_key=''):
         #print('Should all be related:', calculated_value, hist_name, xpeak_name)
             
             
-            
-
-        nrows = len(calculated_value)/2
+    
+        if len(calculated_value) % 2 == 0:
+            nrows = len(calculated_value)//2
+        else:
+            nrows = len(calculated_value)//2 + 1
         #print nrows
         rows =np.arange(nrows)
         #print 'a', rows
@@ -92,31 +94,33 @@ def histogram(path, data_all,table_path, pdf_name , xpeak_key, table_key=''):
     
     
         for aa in range(len(calculated_value)):
-            row = rows[aa/2]
+            row = rows[aa//2]
             col = aa% ncols
             #print row, col
             if aa % (nrows*ncols) ==0:
                 fig, ax_arr = plt.subplots(nrows = nrows, ncols= ncols, squeeze =False)
 
             ax = ax_arr[row,col]
-            min_val = np.nanmin(data_hist[aa])
-            max_val = np.nanmax(data_hist[aa])
+            non_inf = np.where(np.isfinite(data_hist[aa]) == True)[0]
+            min_val = np.nanmin(data_hist[aa][non_inf])
+            max_val = np.nanmax(data_hist[aa][non_inf])
             #print min_val, max_val
         
             bin_arr = np.linspace(min_val, max_val)
             #print bin_arr
         
             title ='Bin: ',ID_detect[aa]
-            ax.hist(data_hist[aa], bins =bin_arr)
+            ax.hist(data_hist[aa][non_inf], bins =bin_arr)
             ax.axvline(x = data_xpeak[aa],color = 'r', label = 'compute_one_sig_xpeak', linewidth = 0.5)
             ax.axvline(x = calculated_value[aa],color = 'm', label = 'stacked metallicities', linewidth =0.5)
             ax.set_xlim(min_val, max_val)
             ax.annotate(title, [0.95,0.5], xycoords = 'axes fraction',va = 'center', ha = 'right', fontsize = 6)
             plt.subplots_adjust(left= 0.07 , bottom= 0.10 , right= 0.97, top= 0.97, wspace = 0.15, hspace =0.15)
-            if aa%(nrows*ncols) == 0:ax.legend(fontsize = 'xx-small')
-            if row != 3:
+            if aa%(nrows*ncols) == 0:
+                ax.legend(title = hist_name, fontsize = 'xx-small')
+            if row != nrows:
                 ax.set_xticklabels([])
-            if row == 3: plt.xlabel(pdf_list[ii]) 
+            #if row == 3: plt.xlabel(pdf_list[ii])
         fig.savefig(pdf_pages, format ='pdf')
         
     pdf_pages.close()
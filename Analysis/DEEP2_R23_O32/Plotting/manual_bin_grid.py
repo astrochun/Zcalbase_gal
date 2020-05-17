a55399c96c12af7e20fcbf81a9553e271ee7276e
Reagen Leimbach <pigdog815@gmail.com>
2020-05-16 23:37:07 -0700

    Added individual spectra to the plot

--- Analysis/DEEP2_R23_O32/Plotting/manual_bin_grid.py
+++ Analysis/DEEP2_R23_O32/Plotting/manual_bin_grid.py
@@ -5,12 +5,13 @@ from astropy.io import ascii as asc
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-#ascii_file = '/Users/reagenleimbach/Desktop/Zcalbase_gal/R23O32_Manual_0514_old/bin_info.tbl'
-#pdf_file = '/Users/reagenleimbach/Desktop/Zcalbase_gal/R23O32_Manual_0514_old/manual_binning_shading.pdf'
+bin_info = '/Users/reagenleimbach/Desktop/Zcalbase_gal/R23O32_Manual_0514_old/bin_info.tbl'
+individual_info = '/Users/reagenleimbach/Desktop/Zcalbase_gal/R23O32_Manual_0514_old/individual_properties.tbl'
+pdf_file = '/Users/reagenleimbach/Desktop/Zcalbase_gal/R23O32_Manual_0514_old/manual_binning_shading.pdf'
 
 n_split = 3
 
-def graph_bins(ascii_file,pdf_file):
+def graph_bins(bin_info, individual_info, pdf_file):
     '''
     Purpose: This function takes inputed grid parameters and graphs them on the log(R23)--log(O32) plane 
     Inputs: 
@@ -22,7 +23,8 @@ def graph_bins(ascii_file,pdf_file):
     Currently works for manual binning with an n_split (see general function) of 3. More updates would need to be made for a different n_split to correct indexing through arrays. 
     '''
     pdf_pages = PdfPages(pdf_file)
-    tab = asc.read(ascii_file)
+    tab = asc.read(bin_info)
+    idv_tab = asc.read(individual_info)
 
     lR23_min = tab['logR23_min'].data
     lO32_min = tab['logO32_min'].data
@@ -30,24 +32,14 @@ def graph_bins(ascii_file,pdf_file):
     lO32_avg = tab['logO32_avg'].data
     lR23_max = tab['logR23_max'].data
     lO32_max = tab['logO32_max'].data
+
+    lR23 = idv_tab['logR23'].data
+    lO32 = idv_tab['logO32'].data
     #print(lR23_min, type(lR23_min))
     
     
     fig, ax = plt.subplots()
-    #finite0 = np.where((np.isfinite(R23)) & (np.isfinite(O32)))[0]
-    #x1 = R23[finite0]
-    #y1 = O32[finite0]
-    #x = np.log10(x1)
-    #y = np.log10(y1)
-    #vlines = np.log10(R23_lowlimit)
-    #hlines = np.log10(O32_lowlimit)
-    #ax.scatter(x,y,1.5, facecolor='r', edgecolor='face', marker='*',alpha=1)
-    ax.set_title(r'$R_{23}$ vs. $O_{32}$ Plot for Manual Binning')
-    ax.set_xlabel(r'log($R_{23}$)')
-    ax.set_ylabel(r'log($O_{32}$)')
-    #for pp in range(n_split*len(bin_start)): plt.axvline(x = vlines[pp], linewidth= 0.3, color= 'k')
 
-    
     count = 0
     for aa in range(len(lR23_min)):
         xmin = lR23_min[aa]
@@ -75,9 +67,26 @@ def graph_bins(ascii_file,pdf_file):
         else:
             ymax = lO32_max[-1]
         print('ymin, ymax = ', lO32_min[aa], ymax)
-        ax.fill_between(xx, lO32_min[aa], ymax)
+        ax.fill_between(xx, lO32_min[aa], ymax, alpha =0.35)
         count+=1
     print(count)
+    
+    finite0 = np.where((np.isfinite(lR23)) & (np.isfinite(lO32)))[0]
+    x1 = lR23[finite0]
+    y1 = lO32[finite0]
+    #x = np.log10(x1)
+    #y = np.log10(y1)
+    #vlines = np.log10(R23_lowlimit)
+    #hlines = np.log10(O32_lowlimit)
+    ax.scatter(x1,y1,0.75, facecolor='r', edgecolor='face', marker='*',alpha=1)
+    ax.set_title(r'$R_{23}$ vs. $O_{32}$ Plot for Manual Binning')
+    ax.set_xlabel(r'log($R_{23}$)')
+    ax.set_ylabel(r'log($O_{32}$)')
+    #for pp in range(n_split*len(bin_start)):
+        #plt.axvline(x = vlines[pp], linewidth= 0.3, color= 'k')
+
+    
+    
     fig.savefig(pdf_pages, format ='pdf')
     pdf_pages.close()
 

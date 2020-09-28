491db83712250e7e0feaeb8b0a12a3ac1edcb18a
Reagen Leimbach <pigdog815@gmail.com>
2020-09-28 16:47:51 -0700

    fixed typo

--- analysis/deep2_r23_o32/__init__.py
+++ analysis/deep2_r23_o32/__init__.py
@@ -5,8 +5,8 @@ name_dict = dict()
 
 name_dict['gridnpz'] = 'grid.npz'
 name_dict['gridpdf'] = 'grid.pdf'
-name_dict['Stackname'] = 'Stacking_Masked_MasterGrid_.pdf'
-name_dict['Stackname_nomask'] = 'Stacking_MasterGrid_.pdf'
+name_dict['Stackname'] = 'Stacking_Masked_MasterGrid.pdf'
+name_dict['Stackname_nomask'] = 'Stacking_MasterGrid.pdf'
 name_dict['Average_Bin_Value'] = '_Average_R23_O32_Values.tbl'
 name_dict['bin_fit_fits'] = 'bin_emission_line_fit.fits'
 name_dict['temp_fits_file'] = '_temperatures_metallicity.fits'

3d8f9323630845bb0e46ce24fe59cb7bda5953b1
Reagen Leimbach <pigdog815@gmail.com>
2021-02-16 16:46:20 -0700

    #85 added parameter dictionary

--- analysis/deep2_r23_o32/run_general_script.py
+++ analysis/deep2_r23_o32/run_general_script.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 from Zcalbase_gal.analysis.deep2_r23_o32 import general
 from Metallicity_Stack_Commons import get_user
+
+params_list = [
+    {'raw': True,  # No MC, no dust, use default valid_table
+     'apply_dust': False,
+     'revised': False},
+    {'raw': True,  # No MC, no dust, use revised valid_table
+     'apply_dust': False,
+     'revised': True},
+    {'raw': True,  # No MC, apply dust, use default valid_table
+     'apply_dust': True,
+     'revised': False},
+    {'raw': True,  # No MC, apply dust, use revised valid_table
+     'apply_dust': True,
+     'revised': True},
+    {'raw': False,  # MC, no dust, use default valid_table
+     'apply_dust': False,
+     'revised': False},
+    {'raw': False,  # MC, no dust, use revised valid_table
+     'apply_dust': False,
+     'revised': True},
+    {'raw': False,  # MC, apply dust, use default valid_table
+     'apply_dust': True,
+     'revised': False},
+    {'raw': False,  # MC, apply dust, use revised valid_table
+     'apply_dust': True,
+     'revised': True},
+]
 dataset = 'n_Bins'
 general.run_grid_r23_o32_analysis(dataset)
 
 fitspath_ini = get_user()
-general.run_grid_plots(fitspath_ini, dataset, raw=False, apply_dust=False,
-                       revised=False, individual=False)
-general.run_grid_plots(fitspath_ini, dataset, raw=False, apply_dust=False,
-                       revised=True, individual=False)
-general.run_grid_plots(fitspath_ini, dataset, raw=True, apply_dust=False,
-                       revised=False, individual=False)
-general.run_grid_plots(fitspath_ini, dataset, raw=True, apply_dust=False,
-                       revised=True, individual=False)
-general.run_grid_plots(fitspath_ini, dataset, raw=False, apply_dust=True,
-                       revised=False, individual=False)
-general.run_grid_plots(fitspath_ini, dataset, raw=False, apply_dust=True,
-                       revised=True, individual=False)
-general.run_grid_plots(fitspath_ini, dataset, raw=True, apply_dust=True,
-                       revised=False, individual=False)
-general.run_grid_plots(fitspath_ini, dataset, raw=True, apply_dust=True,
-                       revised=True, individual=False)
+for params in params_list:
+    general.run_grid_plots(fitspath_ini, dataset, **params, individual=False)

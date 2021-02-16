85341618ca2b34076036a3cbbbf9aaacee6ed851
Chun Ly <astro.chun@gmail.com>
2021-02-16 16:52:47 -0700

    line_ratio_plotting: Fix tab and file namespaces

--- analysis/deep2_r23_o32/plotting/line_ratio_plotting.py
+++ analysis/deep2_r23_o32/plotting/line_ratio_plotting.py
@@ -6,34 +6,34 @@ from matplotlib.backends.backend_pdf import PdfPages
 from os.path import join
 
 
-def plotting_data1(fitspath, dataset, combine_flux_ascii, bin_info):
+def plotting_data1(fitspath, dataset, combine_flux_file, bin_info_file):
     """
     Plots measurements to check to make sure that data follows
     a one-to-one line
 
     :param fitspath: str. path where files are called from and saved to
     :param dataset: str.  keyword used to define binning method
-    :param combine_flux_ascii: str. table of data calculated for emission line
-    :param bin_info: str. table of averages calculated in binning code
+    :param combine_flux_file: str. table of data calculated for emission line
+    :param bin_info_file: str. table of averages calculated in binning code
 
     PDF File: fitspath + 'line_ratio_plots.pdf'
     No returns
     """
 
     pdf_file = join(fitspath, 'line_ratio_plots.pdf')
-    print("### combine_flux_ascii : " + combine_flux_ascii)
-    fitted_data = asc.read(combine_flux_ascii)
+    pp = PdfPages(pdf_file)
 
-    print("### asc_table1 : " + bin_info)
-    raw_data = asc.read(bin_info)
+    print("### combine_flux_file : " + combine_flux_file)
+    fitted_data = asc.read(combine_flux_file)
+
+    print("### bin_info_file : " + bin_info_file)
+    bin_info_tab = asc.read(bin_info_file)
     OII = fitted_data['OII_3727_Flux_Observed']
     OIII5007 = fitted_data['OIII_5007_Flux_Observed']
     H_BETA = fitted_data['HBETA_Flux_Observed']
     binnum = fitted_data['N_stack']
     ID = fitted_data['bin_ID']
     print('binnum:', binnum, len(binnum))
-
-    pp = PdfPages(pdf_file)
 
     R23_composite = np.zeros(binnum.shape[0])
     O32_composite = np.zeros(binnum.shape[0]) 
@@ -44,9 +44,9 @@ def plotting_data1(fitspath, dataset, combine_flux_ascii, bin_info):
         R23_composite[ii] = R23_comp
         O32_composite[ii] = O32_comp
     
-    R23_raw = raw_data['logR23_avg']
-    O32_raw = raw_data['logO32_avg']
-    binnum_raw = raw_data['N_stack']
+    R23_raw = bin_info_tab['logR23_avg']
+    O32_raw = bin_info_tab['logO32_avg']
+    binnum_raw = bin_info_tab['N_stack']
 
     if dataset != 'Grid':
         for rr in range(len(binnum)):
@@ -55,7 +55,7 @@ def plotting_data1(fitspath, dataset, combine_flux_ascii, bin_info):
 
     fig, ax_arr = plt.subplots()
     ax_arr.scatter(R23_raw, R23_composite, marker='o', facecolor='none',
-                   edgecolor='b', label='R23 Ratio: Vornoi Raw vs. Composite')
+                   edgecolor='b', label='R23 Ratio: Voronoi Raw vs. Composite')
     ax_arr.legend(loc=0)
     ax_arr.set_title(dataset + ' Raw vs. Composite for R23')
     for rr in range(len(ID)):
@@ -68,7 +68,7 @@ def plotting_data1(fitspath, dataset, combine_flux_ascii, bin_info):
 
     fig, ax_arr = plt.subplots()
     ax_arr.scatter(O32_raw, O32_composite, marker='o', facecolor='none',
-                   edgecolor='b', label='O32 Ratio: Vornoi Raw vs. Composite')
+                   edgecolor='b', label='O32 Ratio: Voronoi Raw vs. Composite')
     ax_arr.legend(loc=0)
     ax_arr.set_title(dataset + 'Raw vs. Composite for O32')
     for oo in range(len(ID)):

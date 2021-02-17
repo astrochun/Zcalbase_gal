a9b1f504cf5b1d0d3d5a6b37d95fa672667c788c
Chun Ly <astro.chun@gmail.com>
2021-02-16 22:34:24 -0700

    Simplify param_list for readability
     - Make script executable with __name__ check

--- analysis/deep2_r23_o32/run_general_script.py
+++ analysis/deep2_r23_o32/run_general_script.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python3
+
 from Zcalbase_gal.analysis.deep2_r23_o32 import general
 from Metallicity_Stack_Commons import get_user
 
 params_list = [
-    {'raw': True,  # No MC, no dust, use default valid_table
-     'apply_dust': False,
-     'revised': False},
-    {'raw': True,  # No MC, no dust, use revised valid_table
-     'apply_dust': False,
-     'revised': True},
-    {'raw': True,  # No MC, apply dust, use default valid_table
-     'apply_dust': True,
-     'revised': False},
-    {'raw': True,  # No MC, apply dust, use revised valid_table
-     'apply_dust': True,
-     'revised': True},
-    {'raw': False,  # MC, no dust, use default valid_table
-     'apply_dust': False,
-     'revised': False},
-    {'raw': False,  # MC, no dust, use revised valid_table
-     'apply_dust': False,
-     'revised': True},
-    {'raw': False,  # MC, apply dust, use default valid_table
-     'apply_dust': True,
-     'revised': False},
-    {'raw': False,  # MC, apply dust, use revised valid_table
-     'apply_dust': True,
-     'revised': True},
+    # No MC, no dust, use default valid_table
+    {'raw': True, 'apply_dust': False, 'revised': False},
+    # No MC, no dust, use revised valid_table
+    {'raw': True, 'apply_dust': False, 'revised': True},
+    # No MC, apply dust, use default valid_table
+    {'raw': True, 'apply_dust': True, 'revised': False},
+    # No MC, apply dust, use revised valid_table
+    {'raw': True, 'apply_dust': True, 'revised': True},
+    # MC, no dust, use default valid_table
+    {'raw': False, 'apply_dust': False, 'revised': False},
+    # MC, no dust, use revised valid_table
+    {'raw': False, 'apply_dust': False, 'revised': True},
+    # MC, apply dust, use default valid_table
+    {'raw': False, 'apply_dust': True, 'revised': False},
+    # MC, apply dust, use revised valid_table
+    {'raw': False, 'apply_dust': True, 'revised': True},
 ]
-dataset = 'n_Bins'
-general.run_grid_r23_o32_analysis(dataset)
 
-fitspath_ini = get_user()
-for params in params_list:
-    general.run_grid_plots(fitspath_ini, dataset, **params, individual=False)
+
+if __name__ == '__main__':
+    dataset = 'n_Bins'
+
+    general.run_grid_r23_o32_analysis(dataset)
+
+    fitspath_ini = get_user()
+    for params in params_list:
+        general.run_grid_plots(fitspath_ini, dataset, **params,
+                               individual=False)

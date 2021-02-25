fbc8b777c8b26db538f72af73a8ac5b80056dec9
Chun Ly <astro.chun@gmail.com>
2021-02-25 16:53:42 -0700

    Apply suggestions from code review

--- analysis/deep2_r23_o32/binning/n_bins_grid_analysis.py
+++ analysis/deep2_r23_o32/binning/n_bins_grid_analysis.py
@@ -244,13 +244,11 @@ def n_times_binned(fitspath, pdf_pages, npz_outfile, n_split, individual_ID,
 
     # Used to be called +dataset+'_binning_averages.tbl
     bin_outfile = join(fitspath, filename_dict['bin_info'])
-    print(bin_outfile)
     if not exists(bin_outfile):
         log.info(f"Writing: {bin_outfile}")
     else:
         log.info(f"Overwriting : {bin_outfile}")
     asc.write(tab1, bin_outfile, format='fixed_width_two_line', overwrite=True)
-    print('wrote bin_outfile')
 
     n2 = ('logR23', 'logO32', 'OIII_5007_S/N', 'bin_ID', 'ID', 'logR23_min',
           'logO32_min', 'logR23_avg', 'logO32_avg',

e7fe0f2cdcdc173dad2132e3b78f84042489b0be
Reagen Leimbach <pigdog815@gmail.com>
2021-03-19 13:18:23 -0400

    iss #97 first commit of a working draft of curve fitting to analyze LAC plot

---
+++ analysis/deep2_r23_o32/LAC_curvefit_analysis.py
@@ -0,0 +1,130 @@
+import numpy as np
+from astropy.io import ascii as asc
+from scipy.optimize import curve_fit
+from os.path import join
+import matplotlib.pyplot as plt
+from Zcalbase_gal.analysis import local_analog_calibration
+from Metallicity_Stack_Commons.column_names import filename_dict
+
+
+def secondorder_polynomial(x, a, b, c):
+    return a*x*x + b*x + c
+
+
+def thirdorder_polynomial(x, a, b, c, d):
+    return a*x*x*x + b*x*x + c*x +d
+
+
+def experiment_LAC(lR23, lO32, OH, marker, color, pdf_file):
+
+    OH_range = np.linspace(np.min(OH), np.max(OH), 100)
+    p0 = [-0.32293, 7.2954, -54.8284, 138.0430]
+
+    #para_bound = ((working_wave - 3.0, 0.0, 0.0, med0 - 0.05 * np.abs(med0)),
+    # (working_wave + 3.0, 10.0, 100.0, med0 + 0.05 * np.abs(med0)))
+
+    o1, o2 = curve_fit(thirdorder_polynomial, OH, lR23, p0=p0)
+
+    fitted_poly = thirdorder_polynomial(OH_range, *o1)
+    bian_R23 = local_analog_calibration.bian18_R23_OH(OH_range)
+
+    fig, ax = plt.subplots()
+    ax.plot(fitted_poly, OH_range, label='Curve Fit')
+    ax.plot(bian_R23, OH_range, 'k--', label='Bian+(2018)')
+    avail_idx = np.where((OH_range >= 7.80) & (OH_range <= 8.4))[0]
+    ax.plot(bian_R23[avail_idx], OH_range[avail_idx], 'k-', label='Bian Data Range')
+    for ii in range(len(marker)):
+        ax.scatter(lR23[ii], OH[ii], marker=marker[ii], color=color[ii])
+
+    ax.set(xlim=(0.0, 1.2))
+    ax.set_xlabel(r'$\log(R_{23})$')
+    ax.set_ylabel(r'$12+\log({\rm O/H})_{T_e}$')
+    ax.legend(loc='lower left', framealpha=0.5, fontsize=10)
+    
+    fig.savefig(pdf_file)
+
+
+def run_experiment_LAC(fitspath, fitspath_ini, raw=False,
+                  apply_dust=False, revised=False):
+    suffix = ''
+    if not revised:
+        suffix += '.valid1'
+
+    if not raw:
+        suffix += '.MC'
+
+    if apply_dust:
+        suffix += '.dustcorr'
+
+    pdf_file = join(fitspath, f"LAC_curvefit{suffix}.pdf")
+    # Validation Table Call
+    if revised:
+        bin_valid_file = join(fitspath, filename_dict['bin_valid_rev'])
+    else:
+        bin_valid_file = join(fitspath, filename_dict['bin_valid'])
+    bin_valid_tab = asc.read(bin_valid_file)
+
+    bin_derived_prop_file = join(fitspath,
+                                 f"bin_derived_properties{suffix}.tbl")
+    bin_derived_prop_tab = asc.read(bin_derived_prop_file)
+
+    detect = bin_valid_tab['Detection']
+    det_4363 = np.where(detect == 1)[0]
+    rlimit = np.where(detect == 0.5)[0]
+
+    # Tables of individual detections from DEEP2 and MACT samples
+    derived_deep_file = join(fitspath_ini, 'DEEP2_Commons/Catalogs/',
+                             'DEEP2_R23_O32_derived.tbl')
+    derived_deep_tab = asc.read(derived_deep_file)
+
+    derived_mact_file = join(fitspath_ini, 'MACT_Commons/Catalogs/',
+                             'MACT_R23_O32_derived.tbl')
+    derived_mact_tab = asc.read(derived_mact_file)
+
+    # DEEP2 Derived
+    # IDs are not as usefully so passing in a blank array
+    DEEP2_id = []  # derived_deep_tab['ID'].data
+    DEEP2_lR23 = np.log10(derived_deep_tab['R23'].data)
+    DEEP2_lO32 = np.log10(derived_deep_tab['O32'].data)
+    DEEP2_OH = derived_deep_tab['OH'].data
+
+    # MACT Derived
+    # IDs are not as usefully so passing in a blank array
+    MACT_ID = []  # derived_mact_tab['ID'].data
+    MACT_lR23 = np.log10(derived_mact_tab['R23'].data)
+    MACT_lO32 = np.log10(derived_mact_tab['O32'].data)
+    MACT_OH = derived_mact_tab['OH'].data
+
+    ID = bin_derived_prop_tab['bin_ID']
+    lR23_all = bin_derived_prop_tab['logR23_composite']
+    lO32_all = bin_derived_prop_tab['logO32_composite']
+    com_O_log = bin_derived_prop_tab['12+log(O/H)']
+
+    det_ID = ID[det_4363]
+    det_lR23 = lR23_all[det_4363]
+    det_lO32 = lO32_all[det_4363]
+    det_OH = com_O_log[det_4363]
+
+    rlimit_ID = ID[rlimit]
+    rlimit_lR23 = lR23_all[rlimit]
+    rlimit_lO32 = lO32_all[rlimit]
+    rlimit_OH = com_O_log[rlimit]
+
+    # label = ['Detection', 'Robust Limits', 'DEEP2', 'MACT']
+
+    marker_a = ['D'] * len(det_lR23)
+    marker_b = [r'$\uparrow$'] * len(rlimit_lR23)
+    marker_c = ['3'] * len(DEEP2_lR23)
+    marker_d = ['4'] * len(MACT_lR23)
+    marker = np.concatenate([marker_a, marker_b, marker_c, marker_d])
+
+    color_a = ['b'] *len(det_lR23)
+    color_b = ['g']*len(rlimit_lR23)
+    color_c = ['red'] * len(DEEP2_lR23)
+    color_d = ['magenta']*len(MACT_lR23)
+    color = np.concatenate([color_a, color_b, color_c, color_d])
+    lR23 = np.concatenate([det_lR23, rlimit_lR23, DEEP2_lR23, MACT_lR23])
+    lO32 = np.concatenate([det_lO32, rlimit_lO32, DEEP2_lO32, MACT_lO32])
+    OH = np.concatenate([det_OH, rlimit_OH, DEEP2_OH, MACT_OH])
+
+    experiment_LAC(lR23, lO32, OH, marker, color, pdf_file)
